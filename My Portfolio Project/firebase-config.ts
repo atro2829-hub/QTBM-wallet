42015e454b8928248e53d191978b678b4a07dbf1
Mohammed Eyad <atro2829@gmail.com>
2026-03-17 23:49:55 +0000

    Copy all my project keys to a folder named "My Portfolio Project" on the

---
+++ My Portfolio Project/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  "projectId": "qtbm-wallet",
+  "appId": "1:819780958051:android:de6a52b0108f2f97ac4556",
+  "apiKey": "AIzaSyCKQvIXxbnpQJ7uQqpa9pS1Pzhwy1poMnI",
+  "authDomain": "qtbm-wallet.firebaseapp.com",
+  "databaseURL": "https://qtbm-wallet-default-rtdb.firebaseio.com",
+  "storageBucket": "qtbm-wallet.firebasestorage.app",
+  "messagingSenderId": "819780958051"
+};
